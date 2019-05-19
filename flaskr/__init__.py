7dad96254fda620a39ef20defdfbc91d65c437ab
J. Dijkstra <jorryt.dijkstra@ing.com>
2019-05-19 23:56:41 +0200

    Telegram messages and some minor changes

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,8 +2,8 @@ import os
 
 from flask import Flask, g, redirect, render_template, request, url_for
 from apscheduler.schedulers.background import BackgroundScheduler
-import atexit
 from random import randint
+import requests
 
 def create_app(test_config=None):
     # create and configure the app
@@ -33,9 +33,25 @@ def create_app(test_config=None):
     app.register_blueprint(chart.bp)
     app.add_url_rule("/", endpoint="index")
 
+    telegram_enabled = os.environ.get("TELEGRAM_ENABLED", default=False) 
+    telegram_token = os.environ.get("TELEGRAM_TOKEN", default=None)
+    telegram_chatid = os.environ.get("TELEGRAM_CHATID", default=None)
+    polling_interval = int(os.environ.get("POLLING_INTERVAL", default=1800))
+    threshold = 40
+
+    if telegram_enabled:
+        if not telegram_token:
+            print("Warning: Telegram token not specified") 
+
+        if not telegram_chatid:
+            print("Warning: Telegram chat id not specified")
+
+    telegram = telegram_enabled and telegram_token and telegram_chatid
+
     def read_sensors():
         with app.app_context():
-            humidity = randint(0, 80)
+            # TODO: read sensors
+            humidity = randint(0, 100)
             temperature = randint(-20, 50)
             d = db.get_db()
             d.execute(
@@ -44,8 +60,19 @@ def create_app(test_config=None):
             )
             d.commit()
 
+            if telegram and humidity < threshold:
+                send_telegram_msg(humidity)
+
+    def send_telegram_msg(humidity):
+        payload = {'chat_id': telegram_chatid,
+                'text': 'Humidity is currently {}%, which is below the threshold of < {}%'.format(humidity, threshold)}
+        requests.post('https://api.telegram.org/bot{}/sendMessage'.format(telegram_token), data = payload)
+
+
     sched = BackgroundScheduler(daemon=True)
-    sched.add_job(read_sensors, 'interval', minutes=30)
+    sched.add_job(read_sensors, 'interval', seconds=polling_interval)
     sched.start()
 
+    print("Started with polling interval {} seconds and telegram {}".format(polling_interval, "enabled" if telegram else "disabled"))
+    
     return app
