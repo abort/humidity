bd12291f3c64986b1ebc7b302bdc9187b810c89e
J. Dijkstra <jorryt.dijkstra@ing.com>
2019-05-22 13:17:26 +0200

    convert to local timezone

--- flaskr/chart.py
+++ flaskr/chart.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, flash, g, redirect, render_template, request, url_for, current_app as app
 from flaskr.db import get_db
 
-from datetime import datetime
+from datetime import datetime, timezone
 from itertools import groupby, chain
 
 bp = Blueprint("chart", __name__)
@@ -15,7 +15,7 @@ def index():
 	qry = 'SELECT * FROM (SELECT created, humidity, temperature FROM sensor_data ORDER BY created ASC limit 72) ORDER BY created ASC'
 	sensor_data = db.execute(qry).fetchall()
 
-	dates = [row['created'] for row in sensor_data]
+	dates = [row['created'].replace(tzinfo=timezone.utc).astimezone(tz=None) for row in sensor_data]
 	grouped_dates = [ (day, list(values)) for day, values in groupby(dates, extract_day) ]
 	date_labels = [assign_time_label(list(values)) for day, values in grouped_dates]
 
